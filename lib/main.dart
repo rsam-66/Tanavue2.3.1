0db74d6474dae561061482008ae07116edc97c57
rsam-66 <samuelriyanto57@gmail.com>
2025-06-19 17:17:31 +0700

    Update1-working

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+import 'backend/server.dart';
+import 'screens/home_screen.dart';
+import 'screens/monitoring_screen.dart';
+import 'screens/panen_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final server = BackendServer();
+  await server.startServer();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tanavue 2.1',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => const HomeScreen(),
+        '/monitoring': (context) => const MonitoringDataScreen(),
+        '/panen': (context) => const PanenScreen(),
+      },
+    );
+  }
+}
